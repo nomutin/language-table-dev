232bf173555473f242c82bc411b8d217c3bbf51d
nomutin <nomura0508@icloud.com>
2023-09-21 01:46:28 +0900

    :sparkles: Train/validationの分割を自動化

--- language_table_dev/main.py
+++ language_table_dev/main.py
@@ -2,42 +2,18 @@
 
 from pathlib import Path
 
-import numpy as np
 import torch
-from torchvision.transforms import Pad, Resize, ToTensor
-from typing_extensions import Self
+from tqdm import tqdm
 
 from language_table_dev.tf import episode_generator
+from language_table_dev.transform import ActionTransform, ObservationTransform
 
 
-class ActionTransform:
-    """Transforms `np.ndarray` action to `torch.Tensor` actgon."""
-
-    def __call__(self: Self, action: np.ndarray) -> torch.Tensor:
-        """Normalize and tensorize action."""
-        action = action + 0.1
-        action = action * 5.0
-        return torch.from_numpy(action).float()
-
-
-class ObservationTransform:
-    """Transforms `np.ndarray` observation to `torch.Tensor` observation."""
-
-    def __call__(self: Self, observation: np.ndarray) -> torch.Tensor:
-        """Resize and Padding and tensorize observation."""
-        observation_tensor = ToTensor()(observation)
-        mini_observation = Resize((36, 64))(observation_tensor)
-        return Pad((0, 14))(mini_observation)
-
-
-def main() -> None:
+def save_batch(data_dir: Path) -> None:
     """Save action and observation as `torch.Tensor`."""
-    processed_data_path = Path("data/processed")
 
-    action_dir = processed_data_path / "action"
-    observation_dir = processed_data_path / "observation"
-    action_dir.mkdir(parents=True, exist_ok=True)
-    observation_dir.mkdir(parents=True, exist_ok=True)
+    action_dir = data_dir / "action"
+    observation_dir = data_dir / "observation"
 
     for i, episode in enumerate(episode_generator(raw_data_path="data/raw")):
         action_list = []
@@ -45,15 +21,50 @@ def main() -> None:
 
         for step in episode.as_numpy_iterator():
             action = step["action"]
-            observation = step["observation"]["rgb"]
+            action = ActionTransform()(action)
             action_list.append(action)
+            observation = step["observation"]["rgb"]
             observation = ObservationTransform()(observation)
             observation_list.append(observation)
 
-        action_batch = np.stack(action_list)
-        observation_batch = np.stack(observation_list)
+        action_batch = torch.stack(action_list)
+        observation_batch = torch.stack(observation_list)
         torch.save(action_batch, action_dir / f"action_{i}.pt")
         torch.save(observation_batch, observation_dir / f"observation_{i}.pt")
+
+
+def split_train_val(data_dir: Path) -> None:
+    """Split train and validation data."""
+
+    action_dir = data_dir / "action"
+    observation_dir = data_dir / "observation"
+    train_dir = data_dir / "train"
+    val_dir = data_dir / "validation"
+    train_dir.mkdir(parents=True, exist_ok=True)
+    val_dir.mkdir(parents=True, exist_ok=True)
+
+    train_length = 6636
+    val_length = 1660
+
+    for i in tqdm(range(train_length)):
+        action = torch.load(action_dir / f"action_{i}.pt")
+        observation = torch.load(observation_dir / f"observation_{i}.pt")
+        torch.save(action, train_dir / f"action_{i}.pt")
+        torch.save(observation, train_dir / f"observation_{i}.pt")
+
+    for i in tqdm(range(train_length, train_length + val_length)):
+        action = torch.load(action_dir / f"action_{i}.pt")
+        observation = torch.load(observation_dir / f"observation_{i}.pt")
+        torch.save(action, val_dir / f"action_{i - 6636}.pt")
+        torch.save(observation, val_dir / f"observation_{i - 6636}.pt")
+
+
+def main() -> None:
+    """Save action and observation as `torch.Tensor`."""
+    data_dir = Path("data/processed")
+    data_dir.mkdir(parents=True, exist_ok=True)
+    save_batch(data_dir=data_dir)
+    split_train_val(data_dir=data_dir)
 
 
 if __name__ == "__main__":
